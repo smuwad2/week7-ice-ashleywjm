ff2df4cce01bdc5684bb5e03ecede942dbe7f766
ashleywjm <ashley.wong.2024@computing.smu.edu.sg>
2025-10-21 03:35:48 +0000

    edited ex1 and ex2  & ex 3

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,17 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                
+                newItem: ""
+            }   
+        },
+        
+        methods: {
+
+            addItem(){
+                this.items.push(this.newItem);
             }
-            
         }
     }
 </script>
@@ -14,5 +22,15 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for='(item, idx) in items'>
+            {{item}}
+            <button @click="items.splice(idx, 1)">Delete!</button>
+        </li>
+    </ul>
+
+    <input type = 'text' v-model='newItem'></input>
+    <button @click = 'addItem()'>Add</button>
+
    
 </template>
